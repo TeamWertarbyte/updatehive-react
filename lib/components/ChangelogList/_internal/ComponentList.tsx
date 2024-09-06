706a8243805df5c68acd2b0806d28bcaa87b466d
Sascha Becker <s.becker@wertarbyte.com>
2024-09-06 11:59:10 +0200

    style: align spacing for readability

--- lib/components/ChangelogList/_internal/ComponentList.tsx
+++ lib/components/ChangelogList/_internal/ComponentList.tsx
@@ -15,13 +15,17 @@ const ComponentList: React.FC<Props> = ({
   typeColorResolver,
 }) => {
   return (
-    <>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 6,
+      }}
+    >
       {changelogs.map((changelog, index) => (
         <div key={`changelogs-${index}`}>
-          <Box sx={() => ({ mb: 1 })}>
-            <Typography level="h3" sx={() => ({ mr: 1 })}>
-              Version {changelog.version}
-            </Typography>
+          <Box sx={() => ({ mb: 2 })}>
+            <Typography level="h1">Version {changelog.version}</Typography>
             <Divider
               sx={{
                 mt: 1,
@@ -32,42 +36,52 @@ const ComponentList: React.FC<Props> = ({
               <Typography>{changelog.description}</Typography>
             )}
           </Box>
-          {changelog.entries.map((entry) => (
-            <div key={`changelogs-${index}-components-${entry.component}`}>
-              {entry.component && (
-                <Typography level="title-lg">{entry.component}</Typography>
-              )}
-              <List
-                marker={'circle'}
-                sx={() => ({ '--ListItem-minHeight': 20 })}
-              >
-                {entry.changelogs.map((entry, entryIndex) => (
-                  <ListItem
-                    sx={() => ({ p: 0 })}
-                    key={`changelogs-${changelog.version}-entry-${entryIndex}`}
-                  >
-                    <Box sx={() => ({ display: 'flex', flexDirection: 'row' })}>
-                      <Typography
-                        level="title-sm"
-                        sx={() => ({
-                          mr: 1,
-                          color: typeColorResolver(entry.changeType),
-                        })}
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 1,
+            }}
+          >
+            {changelog.entries.map((entry) => (
+              <div key={`changelogs-${index}-components-${entry.component}`}>
+                {entry.component && (
+                  <Typography level="title-lg">{entry.component}</Typography>
+                )}
+                <List
+                  marker={'circle'}
+                  sx={() => ({ '--ListItem-minHeight': 20 })}
+                >
+                  {entry.changelogs.map((entry, entryIndex) => (
+                    <ListItem
+                      sx={{ p: 0 }}
+                      key={`changelogs-${changelog.version}-entry-${entryIndex}`}
+                    >
+                      <Box
+                        sx={() => ({ display: 'flex', flexDirection: 'row' })}
                       >
-                        {changeTypeMapper[entry.changeType]}
-                      </Typography>
-                      <Typography level="body-sm">
-                        {entry.description}
-                      </Typography>
-                    </Box>
-                  </ListItem>
-                ))}
-              </List>
-            </div>
-          ))}
+                        <Typography
+                          level="title-sm"
+                          sx={{
+                            mr: 1,
+                            color: typeColorResolver(entry.changeType),
+                          }}
+                        >
+                          {changeTypeMapper[entry.changeType]}
+                        </Typography>
+                        <Typography level="body-sm">
+                          {entry.description}
+                        </Typography>
+                      </Box>
+                    </ListItem>
+                  ))}
+                </List>
+              </div>
+            ))}
+          </Box>
         </div>
       ))}
-    </>
+    </Box>
   );
 };
 
