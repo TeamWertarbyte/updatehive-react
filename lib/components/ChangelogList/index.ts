5a483d67fedd096425a573370a9d5c4f8e9554b1
Sascha Becker <s.becker@wertarbyte.com>
2024-10-02 14:51:24 +0200

    feat: expose internal ComponentList component for direct rendering of changelog data

--- lib/components/ChangelogList/index.ts
+++ lib/components/ChangelogList/index.ts
@@ -2,3 +2,4 @@ export { GroupBy } from './ChangelogList.types';
 
 export { MinimalChangelogList } from './MinimalChangelogList';
 export { ChangelogList } from './ChangelogList';
+export { ComponentList } from './ComponentList';
